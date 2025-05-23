6ded8940baee5a33d8dcae64423b8d00d64de611
Anh <anhnguyen@students.berkeley.net>
2025-05-22 21:41:52 -0700

    Deleted Build File

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "frc-2025";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 89;
-    public static final String GIT_SHA = "3c26f7d9a40785fb97484b137c88aa2786c1f822";
-    public static final String GIT_DATE = "2025-05-22 20:58:14 PDT";
+    public static final int GIT_REVISION = 7;
+    public static final String GIT_SHA = "8110df5695de76873d082cda48403761c83e54c5";
+    public static final String GIT_DATE = "2025-05-22 21:37:16 PDT";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2025-05-22 21:12:08 PDT";
-    public static final long BUILD_UNIX_TIME = 1747973528059L;
-    public static final int DIRTY = 0;
+    public static final String BUILD_DATE = "2025-05-22 21:39:38 PDT";
+    public static final long BUILD_UNIX_TIME = 1747975178945L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
